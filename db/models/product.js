c8eeba4ac023b775cdd159b5ea32a7c1a7f17906
Luisa de Miranda <lemiranda@gmail.com>
2017-02-28 17:00:05 -0500

    product model first stage complete

--- db/models/product.js
+++ db/models/product.js
@@ -1,1 +1,34 @@
 'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Product = db.define('products', {
+    title: {
+        type: Sequelize.STRING,
+        isUnique: true,
+        notEmpty: true,
+    },
+    description:{
+        type: Sequelize.TEXT,
+        notEmpty: true,
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    inventory: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+    },
+    photo: {
+        type: Sequelize.STRING,
+        defaultValue: 'https://pixabay.com/photo-1752164/',
+    },
+    category: {
+        type: Sequelize.STRING,
+        notEmpty: true,
+    },
+}, {});
+
+module.exports = Product
