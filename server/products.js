6cc0537dd68cdd29859b935993ade743e97a7491
Luisa de Miranda <lemiranda@gmail.com>
2017-03-01 15:08:17 -0500

    one comment per route added

--- server/products.js
+++ server/products.js
@@ -4,11 +4,13 @@ const db = require('APP/db')
 const Product = db.model('products')
 
 module.exports = require('express').Router()
+    //display all available products from db
     .get('/', function(req, res, next){
         Product.findAll()
         .then(products => res.json(products))
         .catch(next)
-        })
+    })
+    //display one product (selected or searched)
     .get('/:id', function(req, res, next){
         Product.findById(req.params.id)
         .then(product => res.json(product))
