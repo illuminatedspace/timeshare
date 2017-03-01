899d84246b43fafd387bfb1a5393ef1b6dc21eef
Liz <lizkristinaphillips@gmail.com>
2017-03-01 11:30:06 -0500

    edits post route for logic to cehck for existing user

--- server/users.js
+++ server/users.js
@@ -6,15 +6,29 @@ const User = db.model('users')
 const {mustBeLoggedIn, forbidden,} = require('./auth.filters')
 
 module.exports = require('express').Router()
-	.get('/', forbidden('only admins can list users'), (req, res, next) => 
+	//lists all users, only for admins
+	.get('/', forbidden('only admins can list users'), (req, res, next) =>
 		User.findAll()
 		.then(users => res.json(users))
 		.catch(next))
-	.post('/', (req, res, next) =>
-		User.create(req.body)
-		.then(user => res.status(201).json(user))
-		.catch(next))
-	.get('/:id', mustBeLoggedIn, (req, res, next) => 
+	//creates a user, will check for an existing user first
+	.post('/', (req, res, next) => {
+		User.findOne( { where: { email: req.body.email} })
+		.then(user => {
+			if (user) {
+				let err = new Error('Email is already associated with an account');
+				err.status = 400;
+				return next(err);
+			}
+			else {
+				return User.create(req.body)
+			}
+		})
+		.then( user => res.status(201).json(user))
+		.catch(next)
+	})
+	//gets a single user
+	.get('/:id', mustBeLoggedIn, (req, res, next) =>
 		User.findById(req.params.id)
 		.then(user => res.json(user))
 		.catch(next))
