3c8e6e7c8b679de0017a8a1605d1e604742fcdec
Liz <lizkristinaphillips@gmail.com>
2017-03-02 12:23:51 -0500

    removes AppContainer in favor of using an App component and relying on Provider to give state to components that utilize connect. closes #11

--- app/containers/AppContainer.js
+++
@@ -1,4 +0,0 @@
-import React, { Component } from 'react'
-import { Router, Route, hashHistory, IndexRedirect } from 'react-router'
-
-import store from '../store'
--- app/main.jsx
+++ app/main.jsx
@@ -3,22 +3,27 @@ import React from 'react'
 import {Router, Route, IndexRedirect, browserHistory} from 'react-router'
 import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
+import store from './store'
 
-import AppContainer from './containers/AppContainer'
 import ProductsContainer from './container/ProductsContainer'
 import ProductContainer from './container/ProductContainer'
 
-import store from './store'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
+import App from './components/App'
 
+import {fetchProducts} from './reducers/products'
+
+function onProductsEnter() {
+  store.dispatch(fetchProducts())
+}
 
 //this is the React.DOM render method
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={AppContainer}>
-        <Route path="/products" component={ProductsContainer} />
+      <Route path="/" component={App}>
+        <Route path="/products" component={ProductsContainer} onEnter={onProductsEnter}/>
         <Route path="/products/:productId" component={ProductContainer} />
         <IndexRedirect to="/products" />
       </Route>
