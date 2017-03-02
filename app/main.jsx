8b8b4ce4051bf248bdf9e3ff2e2c03107845ce17
Liz <lizkristinaphillips@gmail.com>
2017-03-02 11:15:11 -0500

    sets up router in app/main.jsx

--- app/main.jsx
+++ app/main.jsx
@@ -5,28 +5,23 @@ import {render} from 'react-dom'
 import {connect, Provider} from 'react-redux'
 
 import AppContainer from './containers/AppContainer'
+import ProductsContainer from './container/ProductsContainer'
+import ProductContainer from './container/ProductContainer'
 
 import store from './store'
-import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 
-const ExampleApp = connect(
-  ({ auth }) => ({ user: auth })
-) (
-  ({ user, children }) =>
-    <div>
-      <nav>
-        {user ? <WhoAmI/> : <Login/>}
-      </nav>
-      {children}
-    </div>
-)
 
+//this is the React.DOM render method
 render (
   <Provider store={store}>
     <Router history={browserHistory}>
-      <Route path="/" component={AppContainer} />
+      <Route path="/" component={AppContainer}>
+        <Route path="/products" component={ProductsContainer} />
+        <Route path="/products/:productId" component={ProductContainer} />
+        <IndexRedirect to="/products" />
+      </Route>
     </Router>
   </Provider>,
   document.getElementById('main')
