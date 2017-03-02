3f2734ada5b928298834e8afdf07a49d06d71025
Ari Israel <aei225@nyu.edu>
2017-03-02 22:57:49 +0100

    reloaded model files from master

---
+++ db/models/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Require our models. Running each module registers the model into sequelize
+// so any other part of the application could call sequelize.model('User')
+// to get access to the User model.
+
+const User = require('./user')
+const OAuth = require('./oauth')
+const Review = require('./review')
+
+OAuth.belongsTo(User)
+User.hasOne(OAuth)
+Review.belongsTo(User)
+User.hasMany(Review)
+Product.belongsTo(Category)
+Category.hasMany(Product)
+module.exports = {User}
