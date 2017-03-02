20ce2d86afacfb270cc25620b97f142a117f94fd
Liz <lizkristinaphillips@gmail.com>
2017-03-02 17:13:43 -0500

    adds seed data to seed.js for product and category

--- db/seed.js
+++ db/seed.js
@@ -5,9 +5,34 @@ const seedUsers = () => db.Promise.map([
   {name: 'Barack Obama', email: 'barack@example.gov', password: '1234'},
 ], user => db.model('users').create(user))
 
+const seedProducts = () => db.Promise.map([
+{
+  title: 'Spring Break II',
+  description: 'Not ready to go back to school? Don\'t! Just keep breakin\'',
+  price: '1,000',
+  inventory: 100,
+}, {
+  title: 'Skip The Holidays',
+  description: 'Grinch? No worries. Go from Turkey Day to New Year\'s Day. No sweat, Ebenezer!',
+  price: '5,000',
+  inventory: 1000,
+}, {
+  title: '24 Hours of Running',
+  description: 'Want the added long term health benefits of running without the sweat? No problem. Come closer to being forever young.',
+  price: '4,560',
+  inventory: 20300,
+},
+], product => db.model('products').create(product))
+
+const seedCategory = () => db.Promise.map([
+  {name: 'one-offs'},
+  {name: 'deluxe'},
+  {name: 'health benefits'},
+], category => db.model('categories').create(category))
+
 db.didSync
   .then(() => db.sync({force: true}))
   .then(seedUsers)
   .then(users => console.log(`Seeded ${users.length} users OK`))
-  .catch(error => console.error(error))    
+  .catch(error => console.error(error))
   .finally(() => db.close())
