1642c060851c5f0445580ad55d00450e049be632
Liz <lizkristinaphillips@gmail.com>
2017-03-06 09:31:39 -0500

    connects #34

--- app/components/PrimaryNavBar.jsx
+++ app/components/PrimaryNavBar.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from 'react'
 
 import AppBar from 'material-ui/AppBar'
+import FlatButton from 'material-ui/FlatButton'
+import IconButton from 'material-ui/IconButton'
+import ActionShoppingCart from 'material-ui/svg-icons/action/shopping-cart'
+
+const PrimaryNavRight = () => (
+  <div>
+    <FlatButton label="Sign In" />
+    <FlatButton label="Sign Up" />
+    <IconButton><ActionShoppingCart /></IconButton>
+  </div>
+)
 
 const PrimaryNavBar = () => (
   <AppBar
     title="TimeShare"
-    iconElementRight={}
+    iconElementRight={PirmaryNavRight}
   />
 )
 
